fa83a99977a1cc9d012c37b788fa3dd00fa38a6e
Konstantin Kouptsov <konstantin@kouptsov.com>
2019-09-15 11:19:33 -0400

    Add TestFailed exception and improve exception handling

--- src/exceptions.h
+++ src/exceptions.h
@@ -3,11 +3,22 @@
 
 namespace BridgeCrossing {
 namespace Exception {
-    struct TestFileFormat : public std::exception 
+    class TestFileFormat : public std::exception 
     {
-        const char* what() {
-            return "Invalid format of test file";
-        }
+    public:
+        TestFileFormat(std::string filename) : m_filename{filename} {}
+        const char* what() { return m_filename.c_str(); }
+    private:
+        std::string m_filename;
+    };
+
+    class TestFailed : public std::exception 
+    {
+    public:
+        TestFailed(int id) : m_id{ std::to_string(id) } {}
+        const char* what() { return m_id.c_str(); }
+    private:
+        std::string m_id;
     };
 }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -15,37 +15,48 @@ int main(int argc, char *argv[])
         exit(0);
     }
     try {
-        // Load all tests as a vector
+        // Load all test descriptions (YAML documents) as a vector
         std::vector<YAML::Node> tests = YAML::LoadAllFromFile(argv[1]);
         for(auto &t : tests) {
-            // dig down to get individual test parameters
-            if (t.Type() != YAML::NodeType::Map) {
-                throw BridgeCrossing::Exception::TestFileFormat();
+            try {
+                // dig into a single YAML document to get individual test parameters
+                if (t.Type() != YAML::NodeType::Map) {
+                    throw BridgeCrossing::Exception::TestFileFormat(argv[1]);
+                }
+                YAML::Node data = t["test"];
+                if (data.Type() != YAML::NodeType::Map) {
+                    throw BridgeCrossing::Exception::TestFileFormat(argv[1]);
+                }
+
+                // test parameters
+                int test_id = data["test_id"].as<int>();
+                int count = data["hikers"].as<int>();
+                std::vector<double> walk_time = data["walk_time"].as<std::vector<double>>();
+
+                // expected result
+                double crossing_time = data["crossing_time"].as<double>();
+
+                // run the test
+                run_test(test_id, count, walk_time, crossing_time);
             }
-            YAML::Node data = t["test"];
-            if (data.Type() != YAML::NodeType::Map) {
-                throw BridgeCrossing::Exception::TestFileFormat();
+            catch(BridgeCrossing::Exception::TestFailed & e) {
+                // If a test failed for some reason, skip it and continue
+                // with the rest of the tests.
+                std::cerr << "Test " << e.what() << " failed." << std::endl;
             }
-
-            // test parameters
-            int test_id = data["test_id"].as<int>();
-            int count = data["hikers"].as<int>();
-            std::vector<double> walk_time = data["walk_time"].as<std::vector<double>>();
-
-            // expected result
-            double crossing_time = data["crossing_time"].as<double>();
-
-            // run the test
-            run_test(test_id, count, walk_time, crossing_time);
         }
     }
-    catch(YAML::BadFile) {
+    catch(YAML::BadFile &) {
         std::cerr << "Could not open file " << argv[1] << std::endl;
         exit(-1);
     }
-    catch(...) {
-        std::cerr << "Could not parse file " << argv[1] << std::endl;
+    catch(BridgeCrossing::Exception::TestFileFormat & e) {
+        std::cerr << "Could not parse file " << e.what() << std::endl;
         exit(-2);
+    }
+    catch(...) {
+        std::cerr << "Unknown error." << std::endl;
+        exit(-3);
     }
     exit(0);
 }
